74ba5298ef35f806c7ea55301f522d623d9b3024
Abraham E. Hanson <ekenehanson@gmail.com>
2023-12-28 17:00:51 +0100

    Placeholders for email and pword

--- src/Components/Adminpage2Edit/Adminpage2Edit.jsx
+++ src/Components/Adminpage2Edit/Adminpage2Edit.jsx
@@ -168,7 +168,7 @@ const handleOrderChange = (e) => {
         console.log("After Updating Order!!")
   
         if (orderResponse.status === 202) {
-          console.log("Order data sent successfully!!");
+          alert("Order data sent successfully!!");
           toast.success("Order data sent successfully!!");
           setShowModal(true);
         } else {

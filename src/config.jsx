b541f546a9496e3a3ed282d0b82890f7fe6a306f
support@prolianceltd.com <support@prolianceltd.com>
2025-07-09 22:15:49 +0100

    ok

--- src/config.jsx
+++ src/config.jsx
@@ -1,11 +1,11 @@
 const config = {
 
-API_BASE_URL: 'http://127.0.0.1:9090',
-// API_BASE_URL: 'https://cmvp-api-v1.onrender.com',
+// API_BASE_URL: 'http://127.0.0.1:9090',
+API_BASE_URL: 'https://dispatch-app-v1.onrender.com',
 
-WEB_PAGE__URL: 'http://localhost:5173',
+//WEB_PAGE__URL: 'http://localhost:5173',
 
-// WEB_PAGE__URL : 'https://crm-frontend-react.vercel.app',
+WEB_PAGE__URL : 'https://dispatch-app-react-v1.vercel.app',
 
 
 
